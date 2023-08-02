81e63578716819691c147fe2bca723117ecefcf0
Shpetim <shpetimmaliu@gmail.com>
2023-08-02 15:46:47 +0200

    start with feed

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,10 +1,25 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
+import { MoreHorizontal } from "react-feather";
 
 const Feed = () => {
   return (
-    <div>
-      <p className="text-3xl text-red-500 font-bold underline">Feed</p>
+    <div className="container mx-auto w-[600px]">
+      <div className="flex p-4">
+        <img
+          src="https://pbs.twimg.com/profile_images/1391922476167405575/gWl0pM1V_400x400.jpg"
+          className="w-14 rounded-full object-cover"
+        />
+        <div className="w-full px-2">
+          <div className="flex justify-between gap-2">
+            <strong>Shpetim Aliu</strong>
+            <div className="flex justify-between gap-2">
+              <p>4hrs ago</p>
+              <MoreHorizontal />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
