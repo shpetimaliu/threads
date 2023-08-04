c093202a749072f1937b1d759bf46ebe4cbe63a1
Shpetim <shpetimmaliu@gmail.com>
2023-08-04 19:00:50 +0200

    update

--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -17,23 +17,28 @@ const Feed = () => {
           className="w-14 h-14 rounded-full object-cover"
         />
         {/* Header of feed */}
-        <div className="w-full px-2">
+        <div className="w-full px-2 py-4 border-b border-[rgba(97,97,97,1)]">
           <div className="flex justify-between gap-2">
             <strong>Shpetim Aliu</strong>
             <div className="flex justify-between gap-2">
-              <p className="">4hrs ago</p>
+              <p className="text-[rgba(97,97,97,1)]">4hrs ago</p>
               <MoreHorizontal />
             </div>
           </div>
           {/* Body */}
-          <div>
+          <div className="py-2">
             <span>Shpetim Aliu trying to create a clone of threads</span>
           </div>
-          <div className="flex gap-2">
+          <div className="flex gap-2 py-2">
             <Heart size={22} />
             <MessageCircle size={22} />
             <Repeat size={22} />
             <Send size={22} />
+          </div>
+          <div className="flex gap-3">
+            <p className="text-[rgba(97,97,97,1)]">Replies 3</p>
+            <p className="text-[rgba(97,97,97,1)]"> • </p>
+            <p className="text-[rgba(97,97,97,1)]">23 Likes</p>
           </div>
         </div>
       </div>
