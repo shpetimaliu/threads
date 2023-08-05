2ee2937d622ef9910d51ee261086c1c72877800f
Shpetim <shpetimmaliu@gmail.com>
2023-08-05 22:36:20 +0200

    🪄

--- appWriteConfig.js
+++ appWriteConfig.js
@@ -1,9 +1,14 @@
-import { Client } from "appwrite";
+import { Client, Databases } from "appwrite";
 
 const client = new Client();
-const ENDPOINT = import.meta.env.APPWRITE_ENDPOINT;
-const PROJECT_ID = import.meta.env.APPWRITE_SETPROJECT;
 
-client.setEndpoint(ENDPOINT).setProject(PROJECT_ID);
+export const VITE_ENDPOINT = import.meta.env.VITE_APPWRITE_ENDPOINT;
+export const PROJECT_ID = import.meta.env.VITE_APPWRITE_SETPROJECT;
+export const DB_ID = import.meta.env.VITE_DB_ID;
+export const COLLECTION_ID = import.meta.env.VITE_COLLECTION_ID;
+
+client.setEndpoint(VITE_ENDPOINT).setProject(PROJECT_ID);
+
+export const database = new Databases(client);
 
 export default client;
--- src/pages/Feed.jsx
+++ src/pages/Feed.jsx
@@ -1,9 +1,20 @@
 // eslint-disable-next-line no-unused-vars
-import React from "react";
-
+import React, { useEffect, useState } from "react";
 import { Thread } from "../components/Thread";
+import { database, DB_ID, COLLECTION_ID } from "../../appWriteConfig";
 
 const Feed = () => {
+  const [threads, setThreads] = useState();
+
+  useEffect(() => {
+    getThreads();
+  }, []);
+
+  const getThreads = async () => {
+    const response = await database.listDocuments(DB_ID, COLLECTION_ID);
+    console.log("response:", response);
+  };
+
   return (
     <div className="container mx-auto max-w-[600px]">
       <Thread />
