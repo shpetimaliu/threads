9d78b32ef8db9d4f8195b239cb76b72296f35765
Shpetim <shpetimmaliu@gmail.com>
2023-08-22 22:30:07 +0200

    ⚙️

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,22 +1,24 @@
+import { Query } from "appwrite";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { COLLECTION_ID, DB_ID, database } from "../../appWriteConfig";
 
 const Profile = () => {
   const [threads, setThreads] = useState([]);
-  const params = useParams();
+  const { id } = useParams();
 
   useEffect(() => {
     console.log("Params:", params);
-    // getThreads();
+    getThreads();
   });
 
-  //   const getThreads = async () => {
-  //     const response = await database.listDocuments(DB_ID, COLLECTION_ID, [
-  //       Query.orderDesc("$createdAt"),
-  //       Query.equal("owner_id", "Avatar"),
-  //     ]);
-  //     setThreads(response.documents);
-  //   };
+  const getThreads = async () => {
+    const response = await database.listDocuments(DB_ID, COLLECTION_ID, [
+      Query.orderDesc("$createdAt"),
+      Query.equal("owner_id", id),
+    ]);
+    setThreads(response.documents);
+  };
 
   return <div>Profile</div>;
 };
