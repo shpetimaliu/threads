5125a1fc23816603615d637a1bea06183809624a
Shpetim <shpetimmaliu@gmail.com>
2023-09-13 21:45:15 +0200

    use userInformation function to get infor about user

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,37 +1,53 @@
-import React, { useEffect } from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
 import ReactTimeAgo from "react-time-ago";
+import {
+  COLLECTION_ID_PROFILES,
+  DB_ID,
+  database,
+  functions,
+} from "../../appWriteConfig";
 
 // TimeAgo.addDefaultLocale(en);
 
 function Comment({ comment }) {
-  useEffect(() => {}, []);
+  const [loading, setLoading] = useState(true);
+  const [owner, setOwner] = useState(null);
+  useEffect(() => {
+    getUserInformation();
+  }, []);
 
   const getUserInformation = async () => {
     const payload = {
-      owner_id: thread.owner_id,
+      owner_id: comment.owner_id,
     };
     const response = await functions.createExecution(
       "64ced7442bdd27af1d2b",
       JSON.stringify(payload)
     );
+    const profile = await database.getDocument(
+      DB_ID,
+      COLLECTION_ID_PROFILES,
+      comment.owner_id
+    );
     const userData = JSON.parse(response.response);
-    userData["profile_pic"] = user.profile.profile_pic;
-    userData["username"] = user.profile.username;
+    userData["profile_pic"] = profile.profile_pic;
+    userData["username"] = profile.username;
     setOwner(userData);
     setLoading(false);
   };
 
+  if (loading) return;
+
   return (
     <div className="flex p-4">
-      {/* <img
+      <img
         src={owner.profile_pic}
         className="w-14 h-14 rounded-full object-cover"
-      /> */}
+      />
       {/* Header of feed */}
       <div className="w-full px-2 pb-4 border-b border-[rgba(97,97,97,1)]">
         <div className="flex justify-between gap-2">
-          {/* <strong>{owner.name}</strong> */}
+          <strong>{owner.name}</strong>
           <div className="flex justify-between gap-2 items-center cursor-pointer">
             <p className="text-[rgba(97,97,97,1)]">
               <ReactTimeAgo
@@ -43,10 +59,10 @@ function Comment({ comment }) {
           </div>
         </div>
         {/* Body */}
-        <Link to={`/thread/${comment.$id}`}>
-          <div className="py-2 text-white" style={{ whiteSpace: "pre-wrap" }}>
-            {comment.body}
-            {/* <span>
+
+        <div className="py-2 text-white" style={{ whiteSpace: "pre-wrap" }}>
+          {comment.body}
+          {/* <span>
               {comment.image && (
                 <img
                   className="object-cover border border-[rgba(49, 49, 49, 1)] rounded-md"
@@ -54,8 +70,7 @@ function Comment({ comment }) {
                 />
               )}
             </span> */}
-          </div>
-        </Link>
+        </div>
         <div className="flex gap-2 py-2">
           {/* <div className="group flex items-center p-1 rounded-full cursor-pointer hover:hover:bg-[rgba(49,49,49,1)] hover:backdrop-blur-3xl">
             <Heart
